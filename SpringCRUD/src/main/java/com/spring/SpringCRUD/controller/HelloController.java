1b16d4cb013cf87ec3023fddbe08495c0220ffd1
simjeongsoo <simking95@naver.com>
2022-02-13 20:19:23 +0900

    Spring API

--- SpringCRUD/src/main/java/com/spring/SpringCRUD/controller/HelloController.java
+++ SpringCRUD/src/main/java/com/spring/SpringCRUD/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 
 /*
@@ -26,5 +27,43 @@ public class HelloController {
     public String helloMVC(@RequestParam("name") String name,Model model){
         model.addAttribute("name",name);
         return "hello-templete";
+        //http://localhost:8080/hello-mvc?name=spring
     }
+
+    /*API*/
+
+    //@ResponseBody 문자반환
+    @GetMapping("hello-string")
+    @ResponseBody
+    public String helloString(@RequestParam("name") String name){
+
+        return "hello "+ name;
+        //http://localhost:8080/hello-string?name=testtest
+    }
+
+    //@ResponseBody 객체반환
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloapi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+
+
+        return hello;
+        //http://localhost:8080/hello-api?name=test
+    }
+
+    static class Hello{
+        private String name;
+
+        //메서드 접근을 위한 java bean 규약 (property 접근 방식)
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
 }
