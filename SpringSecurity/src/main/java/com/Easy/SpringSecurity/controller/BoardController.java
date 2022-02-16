4e5b78a77c6b10a6c49f616efcef6f914fafe793
simjeongsoo <simking95@naver.com>
2022-02-17 03:12:30 +0900

    form 유효값 체크 Validator 2가지 방법

--- SpringSecurity/src/main/java/com/Easy/SpringSecurity/controller/BoardController.java
+++ SpringSecurity/src/main/java/com/Easy/SpringSecurity/controller/BoardController.java
@@ -2,11 +2,14 @@ package com.Easy.SpringSecurity.controller;
 
 import com.Easy.SpringSecurity.model.Board;
 import com.Easy.SpringSecurity.repository.BoardRepository;
+import com.Easy.SpringSecurity.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @Controller
@@ -15,6 +18,9 @@ public class BoardController {
 
     @Autowired
     private BoardRepository boardRepository;
+
+    @Autowired
+    private BoardValidator boardValidator;
 
     @GetMapping("/list")
     public String list(Model model){
@@ -35,7 +41,11 @@ public class BoardController {
     }
 
     @PostMapping("/form")
-    public String greetingSubmit(@ModelAttribute Board board) {
+    public String greetingSubmit(@Valid Board board, BindingResult bindingResult) {
+        boardValidator.validate(board,bindingResult);
+        if (bindingResult.hasErrors()){
+            return "board/form";
+        }
         boardRepository.save(board);
         return "redirect:/board/list";
     }
