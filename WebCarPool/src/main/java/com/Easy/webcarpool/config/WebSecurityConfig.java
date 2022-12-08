b0d13ad983663a52cbd465c404a6d66c3753ce8c
simjeongsoo <simking95@naver.com>
2022-12-08 16:40:05 +0900

    [jwt] - SpringConfig jwt를 위한 수정

--- WebCarPool/src/main/java/com/Easy/webcarpool/config/WebSecurityConfig.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/config/WebSecurityConfig.java
@@ -1,18 +1,17 @@
 package com.Easy.webcarpool.config;
 
-import com.Easy.webcarpool.jwt.JwtAccessDeniedHandler;
-import com.Easy.webcarpool.jwt.JwtAuthenticationEntryPoint;
-import com.Easy.webcarpool.jwt.JwtSecurityConfig;
-import com.Easy.webcarpool.jwt.TokenProvider;
+import com.Easy.webcarpool.jwt.*;
+import com.Easy.webcarpool.service.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -21,42 +20,56 @@ import javax.sql.DataSource;
 
 @EnableWebSecurity // Spring Security 설정 활성화
 @RequiredArgsConstructor
-@EnableGlobalMethodSecurity(prePostEnabled = true) // @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위해 적용
+@EnableGlobalMethodSecurity(
+        securedEnabled = true,
+        jsr250Enabled = true,
+        prePostEnabled = true  // @PreAuthorize 어노테이션을 메소드 단위로 추가하기 위해 적용
+)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final TokenProvider tokenProvider;
+
+    private final CustomUserDetailsService customUserDetailsService;
     private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
     private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
-    // application.properties 정의된 datasource 사용
     @Autowired
-    private DataSource dataSource;
-
-    @Bean //password 암호화
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-    /* public PasswordEncoder passwordEncoder() 했을때 BeanCurrentlyInCreationException 오류 발생
-    -> inner class의 경우 사용시 static 키워드로 내부클래으세어 바로 사용할 수 있도록 하게 해줌
-    * */
+    private DataSource dataSource; // application.properties 정의된 datasource 사용
 
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        //--h2-console 하위의 모든 요청들과 파비콘 관련 요청은 Spring Security 로직을 수행하지 않고 접근설정--//
-        return web -> web
-                .ignoring()
-                .antMatchers("/h2-console/**", "/favicon.ico");
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception{
+        return super.authenticationManagerBean();
     }
 
+    @Bean
+    public JwtAuthenticationFilter jwtFilter() {
+        return new JwtAuthenticationFilter(tokenProvider);
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder)throws Exception {
+        authenticationManagerBuilder
+                .userDetailsService(customUserDetailsService)
+                .passwordEncoder(passwordEncoder());
+    }
+
+    @Bean
+    public  PasswordEncoder passwordEncoder() {
+        //password 암호화
+        return new BCryptPasswordEncoder();
+    }
 //=========================================================================================
     /**
-     * 접근
+     * Spring Security config method
      * */
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.headers().frameOptions().sameOrigin();
         //http 접근 보안설정
         http
+                .cors().and()
+
                 // token을 사용하는 방식이기 때문에 csrf를 disable
                 .csrf().disable()
 
@@ -73,8 +86,34 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 // 접근 권한 설정
                 .and()
                 .authorizeRequests()
-                .antMatchers("/api/*","/", "/account/register", "/account/*", "/css/**", "/api/**", "/assets/**", "/chat/**").permitAll()
+                .antMatchers("/api/*",
+                        "/",
+                        "/account/register",
+                        "/account/*",
+                        "/css/**",
+                        "/api/**",
+                        "/assets/**",
+                        "/chat/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
+
+                // 추가
+                .antMatchers(
+                        "/favicon.ico",
+                        "/**/*.png",
+                        "/**/*.gif",
+                        "/**/*.svg",
+                        "/**/*.jpg",
+                        "/**/*.html",
+                        "/**/*.css",
+                        "/**/*.js")
+                .permitAll()
+                .antMatchers("/api/auth/**")
+                .permitAll()
+                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
+                .permitAll()
+                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
+                .permitAll()
+
                 .anyRequest().authenticated()
 
                 .and()
@@ -98,7 +137,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * 로그인 입증
      * */
-    @Autowired
+    /*@Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // AuthenticationManagerBuilder:  인증 매니저 생성 빌더
 
         auth.jdbcAuthentication()
@@ -111,7 +150,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         + "from user_role ur inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
                         + "where u.username = ?");
-    }
+    }*/
     /*
      * Authentication 로그인 처리 : usersByUsernameQuery
      * Authorization 권한 처리 : authoritiesByUsernameQuery
