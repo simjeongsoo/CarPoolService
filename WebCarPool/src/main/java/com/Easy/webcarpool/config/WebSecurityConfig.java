74ebc683a993e91b1141a969c9759a8333b71c5f
simjeongsoo <simking95@naver.com>
2022-12-13 23:40:51 +0900

    thymeleaf+SpringSecurity 사용을 위한 WebSecurityConfig 수정

--- WebCarPool/src/main/java/com/Easy/webcarpool/config/WebSecurityConfig.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/config/WebSecurityConfig.java
@@ -1,18 +1,13 @@
 package com.Easy.webcarpool.config;
 
-import com.Easy.webcarpool.jwt.*;
-import com.Easy.webcarpool.jwt.CustomUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -27,35 +22,15 @@ import javax.sql.DataSource;
 )
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final TokenProvider tokenProvider;
-
-    private final CustomUserDetailsService customUserDetailsService;
-    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
-    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
+//    private final TokenProvider tokenProvider;
+//    private final CustomUserDetailsService customUserDetailsService;
+//    private final JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
+//    private final JwtAccessDeniedHandler jwtAccessDeniedHandler;
 
     @Autowired
     private DataSource dataSource; // application.properties 정의된 datasource 사용
-
     @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception{
-        return super.authenticationManagerBean();
-    }
-
-    @Bean
-    public JwtAuthenticationFilter jwtFilter() {
-        return new JwtAuthenticationFilter();
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder)throws Exception {
-        authenticationManagerBuilder
-                .userDetailsService(customUserDetailsService)
-                .passwordEncoder(passwordEncoder());
-    }
-
-    @Bean
-    public  PasswordEncoder passwordEncoder() {
+    public static PasswordEncoder passwordEncoder() {
         //password 암호화
         return new BCryptPasswordEncoder();
     }
@@ -68,26 +43,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.headers().frameOptions().sameOrigin();
         //http 접근 보안설정
         http
-                .cors().and()
 
                 // token을 사용하는 방식이기 때문에 csrf를 disable
                 .csrf().disable()
-
-                // Exception을 핸들링 할때 만들어둔 클래스 추가
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
-                .accessDeniedHandler(jwtAccessDeniedHandler)
-
-                // 세션을 사용하지 않기 때문에 STATELESS로 설정
-                .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-
                 // 접근 권한 설정
-                .and()
                 .authorizeRequests()
-                .antMatchers("/api/*",
-                        "/",
+                .antMatchers("/",
                         "/account/register",
                         "/account/*",
                         "/css/**",
@@ -95,25 +56,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         "/assets/**",
                         "/chat/**").permitAll()
                 .antMatchers("/admin/**").hasAuthority("ROLE_ADMIN")
-
-                // 추가
-                .antMatchers(
-                        "/favicon.ico",
-                        "/**/*.png",
-                        "/**/*.gif",
-                        "/**/*.svg",
-                        "/**/*.jpg",
-                        "/**/*.html",
-                        "/**/*.css",
-                        "/**/*.js")
-                .permitAll()
-                .antMatchers("/api/auth/**")
-                .permitAll()
-                .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability")
-                .permitAll()
-                .antMatchers(HttpMethod.GET, "/api/polls/**", "/api/users/**")
-                .permitAll()
-
                 .anyRequest().authenticated()
 
                 .and()
@@ -123,11 +65,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .successHandler(new LoginSuccessHandler()).permitAll() // 로그인 성공 후 핸들러
 
                 .and()
-                .logout().permitAll()
-
-                // JwtFilter를 addFilterBefore로 등록했던 JwtSecurityConfig 클래스 적용
-                .and()
-                .apply(new JwtSecurityConfig());
+                .logout().permitAll();
     }
 
 //=========================================================================================
@@ -137,7 +75,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     /**
      * 로그인 입증
      * */
-    /*@Autowired
+    @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception { // AuthenticationManagerBuilder:  인증 매니저 생성 빌더
 
         auth.jdbcAuthentication()
@@ -150,7 +88,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                         + "from user_role ur inner join user u on ur.user_id = u.id "
                         + "inner join role r on ur.role_id = r.id "
                         + "where u.username = ?");
-    }*/
+    }
     /*
      * Authentication 로그인 처리 : usersByUsernameQuery
      * Authorization 권한 처리 : authoritiesByUsernameQuery
