6ae125e1b5a54db105cab72d6c7a0dc39ed3c06a
simjeongsoo <simking95@naver.com>
2022-12-14 23:51:34 +0900

    User Entity, UserService 수정

--- WebCarPool/src/main/java/com/Easy/webcarpool/model/User.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/model/User.java
@@ -3,6 +3,7 @@ package com.Easy.webcarpool.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import javax.validation.constraints.Size;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,11 +13,18 @@ import java.util.List;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long id; // pk
 
-    private String username;
-    private String password;
-    private Boolean enable;
+    private String realname; // 실명
+    private String username; // 닉네임(아이디)
+    private String email;    // 이메일
+    private String password; // 비밀번호
+    private Boolean enable;  // 활성화 여부
+    private String address;  // 주소
+    private boolean gender;  // 성별
+    @Size(max = 8)
+    private String birth;    // 생년월일
+
 
     // Role table과 ManyToMany 매핑
     @ManyToMany
@@ -30,7 +38,7 @@ public class User {
 
     // 양방향 매핑, board 클래스 ManyToOne에서 사용한 변수명 "user"
 //    @OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true)
-    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY) //쓸데없는 쿼리 안불러오는 LAZY
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY) //쓸데없는 쿼리 안불러오는 LAZY
 //    @JsonIgnore
     private List<Board> boards = new ArrayList<>();
 }
--- WebCarPool/src/main/java/com/Easy/webcarpool/service/UserService.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/service/UserService.java
@@ -1,30 +1,63 @@
 package com.Easy.webcarpool.service;
 
+import com.Easy.webcarpool.dto.SignUpRequestForm;
 import com.Easy.webcarpool.model.Role;
 import com.Easy.webcarpool.model.User;
 import com.Easy.webcarpool.repository.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 //user 관련 비즈니스 로직 서비스 클래스
 @Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
 
-    @Autowired
-    private PasswordEncoder passwordEncoder;
+    private final PasswordEncoder passwordEncoder;
 
-    public User save(User user){
-        //password 암호화
-        String encodedPassword = passwordEncoder.encode(user.getPassword());
-        user.setPassword(encodedPassword);
-        user.setEnable(true);
+    //--회원 가입--//
+    @Transactional
+    public User join(SignUpRequestForm form){
+
+        User user = new User();
+        user.setRealname(form.getRealname()); // 실명 설정
+        user.setUsername(form.getUsername()); // 아이디 설정
+        user.setEmail(form.getEmail());        // 이메일 설정
+
+        String encodedPassword = passwordEncoder.encode(form.getPassword()); //password 암호화
+        user.setPassword(encodedPassword); // 비밀번호 설정
+
+        user.setEnable(true); // default : 활성화
+
         Role role = new Role();
-        role.setId(2l);// 권한부여
+        role.setId(1L);// default 권한부여 : ROLE_USER
         user.getRoles().add(role);
+
+        validateDuplicatedMember(user); // 중복 회원 검증
         return userRepository.save(user);
     }
+
+    private void validateDuplicatedMember(User user) {
+        // 중복 회원 검증 메서드
+        User findUsers = userRepository.findByUsername(user.getUsername());
+        if (findUsers != null) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+
+    }
+
+    public List<User> findMembers() {
+        return userRepository.findAll();
+    }
+
+    //--단일 회원 조회--//
+//    public User findOne(User user) {
+//        return userRepository.findOne(user.getId());
+//    }
 }
