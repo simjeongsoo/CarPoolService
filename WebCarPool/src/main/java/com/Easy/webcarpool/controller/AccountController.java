c8fd9064d6bb27ee96e75b80ce7f7be1c804177c
simjeongsoo <simking95@naver.com>
2022-12-15 23:49:35 +0900

    AccountController - 자체회원가입 컨트롤러 추가

--- WebCarPool/src/main/java/com/Easy/webcarpool/controller/AccountController.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/controller/AccountController.java
@@ -1,12 +1,17 @@
 package com.Easy.webcarpool.controller;
 
+import com.Easy.webcarpool.dto.SignUpRequestForm;
 import com.Easy.webcarpool.model.User;
 import com.Easy.webcarpool.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/account")
@@ -32,17 +37,23 @@ public class AccountController {
     }
 
     /**
-     * email 회원가입 컨트롤러
-     * */
+     * 회원가입 필수입력사항 폼 생성 컨트롤러
+     */
     @GetMapping("/email_form")
-    public String email() {
-        return "/account/email_form";
+    public String email(Model model) {
+        model.addAttribute("emailForm", new SignUpRequestForm());
+        return "account/signup/email_form";
     }
 
     @PostMapping("/email_form")
-    public String register(User user){
+    public String register(@Valid SignUpRequestForm form, BindingResult result){
         //회원가입시 패스워드 암호화 , 사용자 권한 추가 -> service 클래스 에서 처리
-        userService.save(user);
+
+        if (result.hasErrors()) {
+            return "account/signup/email_form";
+        }
+
+        userService.join(form);
         return "redirect:/";
     }
 
