e65e4b51b906a1c89919a60b7323a8925595c895
simjeongsoo <simking95@naver.com>
2022-12-19 19:51:20 +0900

    UserInfoController - 사용자 프로필 조회 컨트롤러 수정

--- WebCarPool/src/main/java/com/Easy/webcarpool/controller/UserInfoController.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/controller/UserInfoController.java
@@ -1,13 +1,52 @@
 package com.Easy.webcarpool.controller;
 
+import com.Easy.webcarpool.dto.ProfileResponseDto;
+import com.Easy.webcarpool.dto.ProfileUpdateRequestDto;
+import com.Easy.webcarpool.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserInfoController {
 
+    Logger logger = LoggerFactory.getLogger(UserInfoController.class);
+
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 현재 사용자 프로필 조회
+     * */
     @GetMapping("/info")
-    public String userInfo() {
+    public String userInfo(Model model, Authentication authentication) {
+        // 현재 로그인한 사용자의 정보로 조회
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        ProfileResponseDto profileResponseDto = userService.getUserProfile(userDetails.getUsername());
+        model.addAttribute("profile", profileResponseDto);
         return "info/info";
     }
+
+    @PostMapping("/info")
+    public String userInfo(@Valid ProfileUpdateRequestDto profileUpdateRequestDto) {
+//        if (result.hasErrors()) {
+//            return "/info/info";
+//        }
+//        userService.updateProfile(profileUpdateRequestDto);
+
+
+        logger.debug("profileUpdateRequestDto : {}", profileUpdateRequestDto.getAddress());
+        logger.debug("profileUpdateRequestDto : {}", profileUpdateRequestDto.getBirth());
+        logger.debug("profileUpdateRequestDto : {}", profileUpdateRequestDto.getIntroduce());
+        return "/info/info";
+    }
+
 }
