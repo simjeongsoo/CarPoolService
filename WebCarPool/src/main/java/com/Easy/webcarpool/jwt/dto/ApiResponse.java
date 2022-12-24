b189e18deb0cd1bf7b2d27728e1b7fa575bb73f8
simjeongsoo <simking95@naver.com>
2022-12-24 23:26:56 +0900

    회원가입시 사용할 필수 정보 Dto

---
+++ WebCarPool/src/main/java/com/Easy/webcarpool/jwt/dto/ApiResponse.java
@@ -0,0 +1,18 @@
+package com.Easy.webcarpool.jwt.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ApiResponse {
+
+    private Boolean success;
+
+    private String message;
+
+    public ApiResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+}
