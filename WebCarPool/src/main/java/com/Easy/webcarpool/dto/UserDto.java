523b3bdd580fc645a4122e67fdbad2d0a004bf9a
simjeongsoo <simking95@naver.com>
2022-12-26 23:51:38 +0900

    회원가입시 사용할 필수 정보 Dto

---
+++ WebCarPool/src/main/java/com/Easy/webcarpool/dto/UserDto.java
@@ -0,0 +1,28 @@
+package com.Easy.webcarpool.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+    //--회원가입시 사용할 필수 정보 Dto--//
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String membername; // 유저 이름
+
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String email;   // 이메일
+
+    @NotNull
+    private String password; // 비밀번호
+
+
+}
