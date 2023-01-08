2af2df1e9773b5dece7a1228afe4f68bbecf300b
simjeongsoo <simking95@naver.com>
2023-01-08 23:51:36 +0900

    UserInfoService - 프로필이미지 업데이트 로직추가

--- WebCarPool/src/main/java/com/Easy/webcarpool/service/UserInfoService.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/service/UserInfoService.java
@@ -5,16 +5,23 @@ import com.Easy.webcarpool.dto.ProfileUpdateRequestDto;
 import com.Easy.webcarpool.model.User;
 import com.Easy.webcarpool.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserInfoService {
+
+    @Value("${file.path}")
+    private String fileDir;
 
     private final UserRepository userRepository;
 
@@ -36,8 +43,48 @@ public class UserInfoService {
         //        userRepository.save(updateProfile);
 
         User updateProfile = userRepository.findByUsername(username); // dirty checking
-        updateProfile.update(profileUpdateRequestDto.getAddress(), profileUpdateRequestDto.getBirth(), profileUpdateRequestDto.getIntroduce());
+        updateProfile.updateProfile(profileUpdateRequestDto.getAddress(), profileUpdateRequestDto.getBirth(), profileUpdateRequestDto.getIntroduce());
 
+    }
+
+    /**
+     * 프로필 이미지 저장
+    * */
+    @Transactional
+    public Long saveProfileImg(MultipartFile files) throws IOException {
+        if (files.isEmpty()) {
+            return null;
+        }
+
+        // 원래 파일 이름 추출
+        String origName = files.getOriginalFilename();
+
+        // 파일 이름으로 쓸 uuid 생성
+        String uuid = UUID.randomUUID().toString();
+
+        // 확장자 추출(ex : .png)
+        String extension = origName.substring(origName.lastIndexOf("."));
+
+        // uuid와 확장자 결합
+        String savedName = uuid + extension;
+
+        // 파일을 불러올 때 사용할 파일 경로
+        String savedPath = fileDir + savedName;
+
+        // 파일 엔티티 생성
+        User file = User.builder()
+                .profileImgOrgNm(origName)
+                .profileImgSavedNm(savedName)
+                .profileImgSavedPath(savedPath)
+                .build();
+
+        // 실제로 로컬에 uuid를 파일명으로 저장
+        files.transferTo(new File(savedPath));
+
+        // 데이터베이스에 파일 정보 저장
+        User savedProfile = userRepository.save(file);
+
+        return savedProfile.getId();
     }
 
     public List<User> findUsers() {
