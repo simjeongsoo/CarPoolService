264ef7f2205e26ae7b176cac438d64ade3c2b705
simjeongsoo <simking95@naver.com>
2023-01-12 01:23:48 +0900

    modify comment

--- WebCarPool/src/main/java/com/Easy/webcarpool/service/UserInfoService.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/service/UserInfoService.java
@@ -30,6 +30,7 @@ public class UserInfoService {
      * 프로필 조회
      * */
     public ProfileResponseDto getUserProfile(String username) {
+
         User findByUsername = userRepository.findByUsername(username);
         return new ProfileResponseDto().toDto(findByUsername);
 
@@ -40,8 +41,6 @@ public class UserInfoService {
      * */
     @Transactional
     public void updateProfile(ProfileUpdateRequestDto profileUpdateRequestDto , String username) {
-//        User updateProfile = profileUpdateRequestDto.toEntity();
-        //        userRepository.save(updateProfile);
 
         User updateProfile = userRepository.findByUsername(username); // dirty checking
         updateProfile.updateProfile(profileUpdateRequestDto.getAddress(), profileUpdateRequestDto.getBirth(), profileUpdateRequestDto.getIntroduce());
