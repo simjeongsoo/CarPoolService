9f30af5b249d73939da61306dc1051ef79e61b0c
simjeongsoo <simking95@naver.com>
2023-01-18 23:04:15 +0900

    관리자 컨트롤러 view 설정

--- WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
@@ -1,16 +1,38 @@
 package com.Easy.webcarpool.controller;
 
+import com.Easy.webcarpool.model.User;
+import com.Easy.webcarpool.service.admin.AdminService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/admin")
+@RequiredArgsConstructor
 public class AdminController {
+
+    private final AdminService adminService;
 
     @GetMapping("/home")
     public String admin(){
         return "admin/home";
+    }
+
+    @GetMapping("/management")
+    public String adminManagement(Model model){
+
+        List<User> users = adminService.findUsers();
+        model.addAttribute("users", users);
+
+        return "admin/management";
+    }
+    @GetMapping("/usage")
+    public String adminUsage(){
+        return "admin/usage";
     }
 
     @GetMapping("/chat")
