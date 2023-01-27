8cb80dc6b7442338d4318a6615f0c3502478521b
simjeongsoo <simking95@naver.com>
2023-01-27 15:50:47 +0900

    관리자페이지 - 사용자, 관리자 분리하여 model 에 전달

--- WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
@@ -2,8 +2,11 @@ package com.Easy.webcarpool.controller;
 
 import com.Easy.webcarpool.model.Role;
 import com.Easy.webcarpool.model.User;
+import com.Easy.webcarpool.repository.UserRepository;
 import com.Easy.webcarpool.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,7 +19,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class AdminController {
 
+    Logger logger = LoggerFactory.getLogger(AdminController.class);
     private final AdminService adminService;
+
+    private final UserRepository userRepository;
 
     @GetMapping("/home")
     public String admin(){
@@ -29,10 +35,18 @@ public class AdminController {
         List<User> users = adminService.findUsers();
         model.addAttribute("users", users);
 
-        for (User user : users) {
+        for (int i = 0; i < users.size(); i++) {
+            Long id = users.get(i).getId();
+            User user = userRepository.findById(id).get();
             List<Role> roles = user.getRoles();
-            model.addAttribute("roles", roles);
+            logger.debug("user 정보 : {}", user.getId());
+            for (int j = 0; j < roles.size(); j++) {
+                String roleName = roles.get(j).getName();
+                logger.debug("role 정보 : {}", roleName);
+            }
+
         }
+
 
 
         return "admin/management";
