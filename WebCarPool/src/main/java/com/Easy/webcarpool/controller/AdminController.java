4044f429029b671724b59186dc2b31411dd148da
simjeongsoo <simking95@naver.com>
2023-02-02 14:01:16 +0900

    사용자, 관리자 조회 비즈니스 로직 service단에서 처리하도록 수정

--- WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/controller/AdminController.java
@@ -1,33 +1,26 @@
 package com.Easy.webcarpool.controller;
 
 import com.Easy.webcarpool.dto.ProfileResponseDto;
-import com.Easy.webcarpool.model.Role;
 import com.Easy.webcarpool.model.User;
 import com.Easy.webcarpool.repository.UserRepository;
 import com.Easy.webcarpool.service.admin.AdminService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.repository.query.Param;
-import org.springframework.security.core.parameters.P;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
 public class AdminController {
-
-    Logger logger = LoggerFactory.getLogger(AdminController.class);
     private final AdminService adminService;
-
-    private final UserRepository userRepository;
 
     @GetMapping("/home")
     public String admin(){
@@ -37,46 +30,11 @@ public class AdminController {
     @GetMapping("/management")
     public String adminManagement(Model model){
 
-        List<User> users = adminService.findUsers();
-        Map<Long, String> roleMap = new HashMap<>();
-
-        for (int i = 0; i < users.size(); i++) {
-            Long id = users.get(i).getId();
-            User user = userRepository.findById(id).get();
-
-            List<Role> roles = user.getRoles();
-            logger.debug("user 정보 : {}", user.getId());
-
-            for (int j = 0; j < roles.size(); j++) {
-                String roleName = roles.get(j).getName();
-                logger.debug("role 정보 : {}", roleName);
-
-                roleMap.put(user.getId(), roleName);
-            }
-        }
-
-        List<User> userList = new ArrayList<>();
-        List<User> adminList = new ArrayList<>();
-
-        for (int i = 0; i < users.size(); i++) {
-
-            Long id = users.get(i).getId();
-            String roleName = roleMap.get(id);
-
-            if (roleName.equals("ROLE_USER")) {
-                User user = userRepository.findById(id).get();
-                userList.add(user);
-            } else {
-                User user = userRepository.findById(id).get();
-                adminList.add(user);
-            }
-        }
+        List<User> userList = adminService.findUserList();
+        List<User> adminList = adminService.findAdminList();
 
         model.addAttribute("userList", userList);
         model.addAttribute("adminList", adminList);
-        model.addAttribute("users", users);
-        model.addAttribute("roleMap", roleMap);
-
 
         return "admin/management";
     }
--- WebCarPool/src/main/java/com/Easy/webcarpool/service/admin/AdminService.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/service/admin/AdminService.java
@@ -1,14 +1,17 @@
 package com.Easy.webcarpool.service.admin;
 
 import com.Easy.webcarpool.dto.ProfileResponseDto;
+import com.Easy.webcarpool.model.Role;
 import com.Easy.webcarpool.model.User;
 import com.Easy.webcarpool.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,12 +20,78 @@ public class AdminService {
 
     private final UserRepository userRepository;
 
-    public List<User> findUsers() {
+    public List<User> findAllUsers() {
         return userRepository.findAll();
+    }
+
+    public List<User> findUserList() {
+
+        List<User> users = findAllUsers();
+        Map<Long, String> roleMap = new HashMap<>();
+        List<User> userList = new ArrayList<>();
+
+        for (User value : users) {
+            Long id = value.getId();
+            User user = userRepository.findById(id).get();
+
+            List<Role> roles = user.getRoles();
+
+            for (Role role : roles) {
+                String roleName = role.getName();
+
+                roleMap.put(user.getId(), roleName);
+            }
+        }
+
+        for (User value : users) {
+
+            Long id = value.getId();
+            String roleName = roleMap.get(id);
+
+            if (roleName.equals("ROLE_USER")) {
+                User user = userRepository.findById(id).get();
+                userList.add(user);
+            }
+        }
+
+        return userList;
+    }
+
+    public List<User> findAdminList() {
+        List<User> users = findAllUsers();
+        Map<Long, String> roleMap = new HashMap<>();
+        List<User> adminList = new ArrayList<>();
+
+        for (User value : users) {
+            Long id = value.getId();
+            User user = userRepository.findById(id).get();
+
+            List<Role> roles = user.getRoles();
+
+            for (Role role : roles) {
+                String roleName = role.getName();
+
+                roleMap.put(user.getId(), roleName);
+            }
+        }
+
+        for (User value : users) {
+
+            Long id = value.getId();
+            String roleName = roleMap.get(id);
+
+            if (roleName.equals("ROLE_ADMIN")) {
+                User user = userRepository.findById(id).get();
+                adminList.add(user);
+            }
+        }
+
+        return adminList;
     }
 
     public ProfileResponseDto findUserInfo(Long id) {
         User user = userRepository.findById(id).get();
         return new ProfileResponseDto().toDto(user);
     }
+
 }
