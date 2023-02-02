a28bef7978b5cf7e9bc0523e2e069a1ba1dbe125
simjeongsoo <simking95@naver.com>
2023-02-02 23:56:59 +0900

    RoleRepository 추가

---
+++ WebCarPool/src/main/java/com/Easy/webcarpool/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.Easy.webcarpool.repository;
+
+import com.Easy.webcarpool.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+}
