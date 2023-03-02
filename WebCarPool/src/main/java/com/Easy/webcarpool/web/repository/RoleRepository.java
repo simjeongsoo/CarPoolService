96c6737474c1dc80e80be7997f8135998ed7bae0
simjeongsoo <simking95@naver.com>
2023-03-02 23:04:14 +0900

    [add] - RoleRepository에 findByName 메소드 추가

--- WebCarPool/src/main/java/com/Easy/webcarpool/web/repository/RoleRepository.java
+++ WebCarPool/src/main/java/com/Easy/webcarpool/web/repository/RoleRepository.java
@@ -3,6 +3,9 @@ package com.Easy.webcarpool.web.repository;
 import com.Easy.webcarpool.web.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
 
 }
