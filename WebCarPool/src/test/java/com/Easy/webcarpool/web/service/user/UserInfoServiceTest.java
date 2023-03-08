7c20c3418121a0b837becf7e0c9aa4f33d62674b
simjeongsoo <simking95@naver.com>
2023-03-08 23:22:39 +0900

    [구현] - UserInfoServiceTest 테스트 구현

---
+++ WebCarPool/src/test/java/com/Easy/webcarpool/web/service/user/UserInfoServiceTest.java
@@ -0,0 +1,51 @@
+package com.Easy.webcarpool.web.service.user;
+
+import com.Easy.webcarpool.web.domain.User;
+import com.Easy.webcarpool.web.dto.ProfileResponseDto;
+import com.Easy.webcarpool.web.repository.UserRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class UserInfoServiceTest {
+
+    @InjectMocks
+    private UserInfoService userInfoService;
+    @Mock
+    private UserRepository userRepository;
+
+//    @BeforeEach
+//    public void setUp() {
+////        MockitoAnnotations.initMocks(this);
+//        MockitoAnnotations.openMocks(this);
+//        ReflectionTestUtils.setField(userInfoService, "fileDir", "./test/profileImg/");
+//        ReflectionTestUtils.setField(userInfoService, "carFileDir", "./test/carImg/");
+//    }
+
+    @Test
+    @DisplayName("getUserProfile 메소드 테스트")
+    public void getUserProfile() {
+        // given
+        String username = "testuser";
+        User user = User.builder().username("test").realname("realname").email("email@gmail.com").build();
+        when(userRepository.findByUsername(username)).thenReturn(Optional.of(user));
+
+        // when
+        ProfileResponseDto result = userInfoService.getUserProfile(username);
+
+        // then
+        assertEquals(user.getUsername(), result.getUsername());
+    }
+
+
+}
